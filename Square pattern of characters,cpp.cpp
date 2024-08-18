f277b9a22a9455e196632c22e3f0ab23e58158a9
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2024-08-19 00:12:39 +0530

    Square pattern of characters

---
+++ Square pattern of characters,cpp.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+ using namespace std;
+ int main(){
+	int n;
+	cout<<" enter number : ";
+	cin>>n;
+	
+	for(int i =1 ; i<=n; i++){
+		
+		for(int j=1 ;j<=n;j++){
+			cout<<(char)(i+64);
+		}
+		cout<<endl;
+	}
+}
+
