440e1218b7c925e24945218078bda4de2cb69c54
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2024-08-19 00:13:23 +0530

    Star rectangle partern

---
+++ Star rectangle partern.cpp
@@ -0,0 +1,19 @@
+// star rectangle partern
+// ******
+// ******
+// ******
+// ******
+
+
+#include <iostream>
+ using namespace std;
+ int main(){
+
+	for(int i =1 ; i<=4; i++){ 
+		
+		for(int j=1 ;j<=6;j++){ 
+			cout<<"*";
+		}
+		cout<<endl;
+	}
+}
