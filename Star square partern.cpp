be2e27d64ca38700e5b6ed66aeb1073a74ce5978
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2024-08-19 00:14:17 +0530

    Star square partern

---
+++ Star square partern.cpp
@@ -0,0 +1,17 @@
+// star square partern 
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter the size of the square: ";
+    cin>>n;
+
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n; j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
