c399a13ab534d6cef53aad95a3f40f5a55ac0db2
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2024-09-03 20:16:14 +0530

    Number perten 03

---
+++ Number perten 03.cpp
@@ -0,0 +1,23 @@
+ // 04 print parterns
+ 
+ #include <iostream>
+ using namespace std;
+ int main(){
+	int n;
+	cout<<" enter number : ";
+	cin>>n;
+	
+	for(int i =1 ; i<=n; i++){
+		
+		for(int j=1 ;j<=n;j++){
+			cout<<j;
+		}
+		cout<<endl;
+	}
+}
+
+//n =4
+//1111
+//2222
+//3333
+//4444
